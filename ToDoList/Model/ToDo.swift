2c0e011159078e98dd3d2dfc29fe35a6676c98bc
Mathew <asobu.developers+mathew@gmail.com>
2022-10-20 11:55:11 +0900

    ToDo Modelの構造を記述・初期値の宣言

--- ToDoList/Model/ToDo.swift
+++ ToDoList/Model/ToDo.swift
@@ -1,1 +1,13 @@
 import Foundation
+
+class ToDo {
+    let title: String
+    let content: String
+    let createDate: Date
+    
+    init(initTodoTitle: String, initTodoContent: String, initCreateDate: Date){
+        title = initTodoTitle
+        content = initTodoContent
+        createDate = initCreateDate
+    }
+}
